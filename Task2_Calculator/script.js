cef5ad26aa7f1b939e79142957c60c5f11c3be2b
Suraj Girhe <surajgirhe29@gmail.com>
2026-06-05 14:39:29 +0530

    Update script.js

--- Task2_Calculator/script.js
+++ Task2_Calculator/script.js
@@ -1,1 +1,43 @@
+let display = document.getElementById("display");
 
+// Add value
+function appendValue(value) {
+    display.value += value;
+}
+
+// Clear screen
+function clearDisplay() {
+    display.value = "";
+}
+
+// Backspace
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Calculate result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+// Keyboard support
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+
+    if (!isNaN(key) || "+-*/.".includes(key)) {
+        appendValue(key);
+    } 
+    else if (key === "Enter") {
+        calculate();
+    } 
+    else if (key === "Backspace") {
+        backspace();
+    } 
+    else if (key === "Escape") {
+        clearDisplay();
+    }
+});
