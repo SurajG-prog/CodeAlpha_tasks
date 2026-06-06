075cb736df50815ac90a814150b524e6c8ca1b8c
Suraj Girhe <surajgirhe29@gmail.com>
2026-06-06 22:59:34 +0530

    Update script.js

--- Task3_MyPortfolio/script.js
+++ Task3_MyPortfolio/script.js
@@ -1,1 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
 
+window.addEventListener("scroll", () => {
+
+    reveals.forEach(section => {
+
+        const top = section.getBoundingClientRect().top;
+        const height = window.innerHeight;
+
+        if(top < height - 100){
+            section.classList.add("active");
+        }
+
+    });
+
+});
